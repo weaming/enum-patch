3963e9b0dbbffa6ad3f05456e12cd0d13e231997
weaming <garden.yuen@gmail.com>
2019-05-08 03:43:39 +0800

    add iter_value

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-Patch the python3's `enum.Enum`.
-
-```
-from enum import Enum
-from enum_patch import patch
-patch()
-
-
-class Currency(Enum):
-    USD = 1
-    HKD = 2
-
-
-print("HKD" in Currency)
-print(list(Currency.iter_str()))
-```
+Patch the python3's `enum.Enum`. See `test.py` for usage.
--- enum_patch.py
+++ enum_patch.py
@@ -1,11 +1,16 @@
 from enum import EnumMeta
 
-version = "0.2"
+version = "0.3"
 
 
-def _iter_str(cls):
+def _iter_name(cls):
     for x in cls._member_map_:
         yield x
+
+
+def _iter_value(cls):
+    for x in cls._member_map_.values():
+        yield x.value
 
 
 _origin_contains = EnumMeta.__contains__
@@ -14,9 +19,10 @@ _origin_contains = EnumMeta.__contains__
 def _contains(cls, member):
     if isinstance(member, str):
         return member in cls._member_map_
-    return _origin_contains(member)
+    return _origin_contains(cls, member)
 
 
 def patch():
     EnumMeta.__contains__ = _contains
-    EnumMeta.iter_str = _iter_str
+    EnumMeta.iter_name = _iter_name
+    EnumMeta.iter_value = _iter_value
